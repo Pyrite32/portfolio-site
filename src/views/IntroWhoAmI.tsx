15b5418de3d0d12e594225e73cf3c1c4ebf4dcab
Pyrite32 <patrick@method93.com>
2024-09-21 16:10:21 -0700

    added logos

--- src/views/IntroWhoAmI.tsx
+++ src/views/IntroWhoAmI.tsx
@@ -5,22 +5,26 @@ const IntroWhoAmI = () => {
   return (
     <section className="intro bg-black grid-bg-intro h-full flex flex-col justify-center">
       <div className="intro-main flex-grow flex flex-col h-2/4 gap-28 justify-end">
-        <div className="intro-text mx-auto flex flex-col w-9/12 gap-6">
-          <div className="flex flex-col gap-3  mx-auto">
-            <h1 className="text-7xl text-white">
-              <span>Patrick is a </span>
-              <span>Unicorn </span>
-              <span>🦄</span>
-            </h1>
-            <p className="w-10/12 leading-snug text-2xl text-white">
-              As a unicorn, I have passion for building beautiful and functional
-              digital experiences. I bridge the gap between aesthetics and
-              technology, ensuring every project I am involved in is
+        <div className="intro-text flex gap-4">
+          <div className="text-right text-7xl w-2/12">🦄</div>
+          <div className="mr-auto flex flex-col w-9/12 gap-6">
+            <div className="flex flex-col gap-3  mx-auto">
+              <h1 className="text-7xl text-white">
+                <span>Patrick is a </span>
+                <span>Unicorn </span>
+                
+              </h1>
+              <p className="w-10/12 leading-snug text-2xl text-white">
+                As a unicorn, I have passion for building beautiful and
+                functional digital experiences. I bridge the gap between
+                aesthetics and technology, ensuring every project I am involved
+                in is
+              </p>
+            </div>
+            <p className="font-pixel text-2xl">
+              <span className="text-yellow">$</span> list skills
             </p>
-          </div>
-          <p className="font-pixel text-2xl">
-            <span className="text-yellow">$</span> list skills
-          </p>
+          </div>   
         </div>
         <div className="w-11/12 mr-auto text-right">
           <ul className="font-pixel text-2xl">
