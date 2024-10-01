7e0e426dbf90f3e2656b61085d782c54d32a28dd
Pyrite32 <patrick@method93.com>
2024-10-01 14:21:27 -0700

    icons are here

--- src/components/CourseworkTile.tsx
+++ src/components/CourseworkTile.tsx
@@ -1,11 +1,32 @@
 import { useState } from "react";
 import './CourseworkTile.css'
 
+import dsaIcon from '../assets/course-icons/dsa.svg'
+import japanIcon from '../assets/course-icons/japan.svg'
+import linearAlgebraIcon from '../assets/course-icons/linearAlgebra.svg'
+import operatingSystemsIcon from '../assets/course-icons/operatingSystems.svg'
+import performantProgrammingIcon from '../assets/course-icons/performantProgramming.svg'
+import plcIcon from '../assets/course-icons/plc.svg'
+import taIcon from '../assets/course-icons/ta.svg'
+import uxIcon from '../assets/course-icons/ux.svg'
+
 export interface CourseworkData {
     code: string,
     name: string,
     desc: string,
+    icon: string
 }
+const iconToSVG: Map<string, string> = new Map([
+    ['dsa', dsaIcon],
+    ['japan', japanIcon],
+    ['linearAlgebra', linearAlgebraIcon],
+    ['operatingSystems', operatingSystemsIcon],
+    ['performantProgramming', performantProgrammingIcon],
+    ['plc', plcIcon],
+    ['ta', taIcon],
+    ['ux', uxIcon]
+]);
+
 
 const CourseworkTitle = (props: {data: CourseworkData, borderStyle: string}) => {
     const [hovered, setHovered] = useState(false);
@@ -14,14 +35,17 @@ const CourseworkTitle = (props: {data: CourseworkData, borderStyle: string}) => 
         <div 
         onMouseEnter={() => setHovered(true)}
         onMouseLeave={() => setHovered(false)}
-        className="max-w-1/4 bg-white px-7 py-5 font-pixel "
+        className="max-w-1/4 bg-white px-7 py-5 font-pixel flex flex-col items-center justify-center"
         >
-            <div className="text-center">
-                {props.data.code}
-            </div>
-            <div className="text-center">
-                {props.data.name}
-            </div>
+                <div className="mx-auto w-max flex-grow">
+                    <img className="my-auto align-middle pt-12 bg-cover" width="48" src={iconToSVG.get(props.data.icon)}  style={{filter: "brightness(4%)"}} alt="" />
+                </div>
+                {/* <div className="text-center text-2xl">
+                    {props.data.code}
+                </div>
+                <div className="text-center text-2xl">
+                    {props.data.name}
+                </div> */}
         </div>
     )
 }
--- src/views/CourseworkShowcase.tsx
+++ src/views/CourseworkShowcase.tsx
@@ -6,49 +6,49 @@ const courses = [
     code: "COP 4600",
     name: "Operating Systems",
     desc: "Piping, networking, multithreading, OS principles, and modification of a custom Unix kernel in C.",
-    icon: "2"
+    icon: "operatingSystems",
   },
   {
     code: "CEN 4930",
     name: "Performant Programming",
     desc: "Programming with Rust and Python. Learned Rust + Unity bindings, multithreading, parallel processing, and async I/O",
-    icon: "2"
+    icon: "performantProgramming",
   },
   {
     code: "COP 3530",
     name: "Data Structures & Algorithms",
     desc: "Custom implementation of various data structures and sorting/pathing algorithms in C++. Learned how to create linked lists, stacks, queues, BSTs, ASTs, graphs, and implement BFS, DFS, Djikstra, and A* algorithms",
-    icon: "2"
+    icon: "dsa",
   },
   {
     code: "MAS 3114",
     name: "Computational Linear Algebra",
     desc: "Vector and matrix math with MATLAB. Learned eigenvalues, echelon forms, vector spaces, and vector transformations.",
-    icon: "2"
+    icon: "linearAlgebra"
   },
   {
     code: "COP 4020",
     name: "Programming Language Concepts",
     desc: "OOP, regex, and creation of a custom programming language using Java. Learned regex, parsing, lexing, and object oriented programming.",
-    icon: "2"
+    icon: "plc"
   },
   {
     code: "CEN 4722",
     name: "UX Design",
     desc: "Learned how to implement UX through Figma. Learned about user stories, responsive design, principles of user-friendly design, and color theory.",
-    icon: "2"
+    icon: "ux"
   },
   {
     code: "DIG 4942",
     name: "Undergraduate Course Assistant",
     desc: "Assisted students in building cut-out animations with ToonBoom Harmony. Performed weekly assessment of student submissions with SyncSketch.",
-    icon: "2"
+    icon: "ta"
   },
   {
     code: "CEN 4930",
     name: "Cross Cultural Engineering & Design",
-    desc: "Collaborated in a team of six to create a unity game centered around disaster prevention.",
-    icon: "2"
+    desc: "Collaborated in a team of six in Kyoto, Japan to create a unity game centered around disaster prevention.",
+    icon: "japan"
   }
 ]
 
